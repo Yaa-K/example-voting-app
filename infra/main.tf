fd2e563189bc5aa3e28f1312d0e83cbf07a38872
Yaa-K <yaa.k.yeboah@outlook.com>
2026-08-28 22:11:27 +0000

    Add fourth node for monitoring

--- infra/main.tf
+++ infra/main.tf
@@ -51,8 +51,8 @@ module "eks" {
   eks_managed_node_groups = {
     default = {
       min_size       = 2
-      max_size       = 3
-      desired_size   = 3
+      max_size       = 4
+      desired_size   = 4
       instance_types = ["t3.micro"]
     }
   }
